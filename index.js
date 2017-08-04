b669fa9100672f86e32f963c1419da968844fb4f
NOK <nokkanato@gmail.com>
2017-08-05 01:08:37 +0700

    heroku

--- index.js
+++ index.js
@@ -46,7 +46,9 @@ function handleEvent(event) {
 }
 
 // listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
+app.set('port', process.env.PORT || 5000);
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
