c9e5e82e551f101d02b3208959c5a84aca34c5de
NOK <nokkanato@gmail.com>
2017-08-05 01:05:53 +0700

    heroku

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
+
+app.post('/', (req, res) => {
+  res.send('success')
+})
+
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
